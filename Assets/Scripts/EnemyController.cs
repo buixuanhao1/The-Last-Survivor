99394ce42b40203e4348401a107c06d2f6e5af70
Bùi Xuân Hào <hbui40537@gmail.com>
2025-09-11 13:48:56 +0700

    Add EnemyData ScriptableObject and EnemyController

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public EnemyData enemyData;
+    private Transform player;
+    [SerializeField] private SpriteRenderer rbSprite;
+    private int currentHP;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentHP = enemyData.baseHP;
+
+        // Gán sprite từ data
+        GetComponent<SpriteRenderer>().sprite = enemyData.sprite;
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        // Di chuyển về phía player
+        Vector2 dir = (player.position - transform.position).normalized;
+        if (dir.x < 0)
+        {
+            rbSprite.flipX = true;
+        }else if (dir.x > 0)
+        {
+            rbSprite.flipX=false;
+        }
+        transform.position += (Vector3)dir * enemyData.moveSpeed * Time.deltaTime;
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+        if (currentHP <= 0) Die();
+    }
+
+    void Die()
+    {
+        // TODO: drop exp orb ở đây
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [Header("Thông tin cơ bản")]
+    public string enemyName;
+    public int baseHP;
+    public float moveSpeed;
+    public int damage;
+    public int expDrop;
+
+    [Header("Hình ảnh")]
+    public Sprite sprite;
+}
