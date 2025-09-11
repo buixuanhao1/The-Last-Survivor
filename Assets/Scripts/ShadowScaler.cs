04a0b035439171094672c23b4f56afa9431bd324
Bùi Xuân Hào <hbui40537@gmail.com>
2025-09-11 11:42:43 +0700

    add shadow for player, joystick, background

---
+++ Assets/Scripts/ShadowScaler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ShadowScaler : MonoBehaviour
+{
+    public Transform target;  // Player hoặc Enemy
+    public Vector3 baseScale = new Vector3(0.3823f, 0.3506f, 1f);
+
+    void Update()
+    {
+        if (target != null)
+        {
+            float scaleFactor = target.localScale.x;
+            transform.localScale = baseScale * scaleFactor;
+        }
+    }
+}
