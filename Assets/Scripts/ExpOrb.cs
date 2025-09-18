99fe42226489e11ae00646033ddcbb702c67c5ee
Bùi Xuân Hào <hbui40537@gmail.com>
2025-09-18 23:10:45 +0700

    .

---
+++ Assets/Scripts/ExpOrb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ExpOrb : MonoBehaviour
+{
+    private int expAmount;
+
+    public void SetExpAmount(int amount)
+    {
+        expAmount = amount;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerExperience exp = other.GetComponent<PlayerExperience>();
+            if (exp != null)
+            {
+                exp.AddExp(expAmount);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
