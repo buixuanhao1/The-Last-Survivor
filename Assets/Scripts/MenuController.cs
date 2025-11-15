e27cb5db3dc3d123e5e958310d324a3cd5ff87c9
Bùi Xuân Hào <hbui40537@gmail.com>
2025-11-15 11:50:39 +0700

    Revert "rearrange code"
    This reverts commit 3c4d6562410b8f79a693f50c4224e96b34e2233c.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    // Hàm này sẽ gọi khi nhấn nút Play
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+}
